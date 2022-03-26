2ccb97afd9617ea5361af089bae3aeba7f074c75
Robert-App-Dev <robert01.appdev@gmail.com>
2022-03-26 16:38:29 +0000

    Added support for creating singular setup exe file

--- BUILD.bat
+++ BUILD.bat
@@ -3,3 +3,4 @@ pyinstaller main.pyw --exclude-module _bootlocale -y
 copy VERSION.txt dist\main\VERSION.txt
 Xcopy HelpText dist\main\HelpText\ /E /H /C /I
 mkdir dist\main\Data
+"C:\Program Files (x86)\Inno Setup 6\iscc.exe" InnoSetupCompileScript.iss
